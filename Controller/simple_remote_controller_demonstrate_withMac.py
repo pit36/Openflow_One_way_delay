189deb8669c4e03894a90e08598013c06d1a27cb
ps <you@example.com>
2020-10-21 20:16:16 +0200

    mod ctrl

--- Controller/simple_remote_controller_demonstrate_withMac.py
+++ Controller/simple_remote_controller_demonstrate_withMac.py
@@ -141,7 +141,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         self.datapath_list = {}
         self.arp_table = {}
         self.switches = [] #1
-        self.hosts = {'90:1b:0e:cb:8d:94': (158445886737823, 4294967294), '90:1b:0e:cb:8d:9f': (158445886737812, 4294967294)}
+        self.hosts = {}#{'90:1b:0e:cb:8d:94': (158445886737823, 4294967294), '90:1b:0e:cb:8d:9f': (158445886737812, 4294967294)}
         self.multipath_group_ids = {}
         self.group_ids = []
         self.adjacency = defaultdict(dict)
